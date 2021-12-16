308ce1d3d6f54709827764c903c7b738f036f344
Jon Lane <jlane@prepkc.org>
2021-12-16 10:34:28 -0600

    Working on Express Server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,49 @@
+const fs = require('fs');
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+let aboutMessage = "Issue Tracker API v1.0";
+
+const projectDB = [
+  {
+      id: 1, status: 'new', owner: 'Jon', effort: 5, due: new Date('2018-08-08'),
+      title: 'Refactoring Code for Modularization',
+  },
+  {
+      id: 2, status: 'assigned', owner: 'Jon', effort: 18, due: new Date('2018-08-08'),
+      title: 'Standards and Style Guide',
+  },
+];
+
+const resolvers = {
+  Query: {
+    about: () => aboutMessage,
+    projectList,
+  },
+  Mutation: {
+    setAboutMessage,
+  },
+};
+
+function setAboutMessage(_, { message }) {
+  return aboutMessage = message;
+}
+
+function projectList(){
+  return projectDB;
+}
+
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
+  resolvers,
+});
+
 const app = express();
-const fileServerMiddleware = express.static('public');
-app.use('/', fileServerMiddleware);//First argument is optional, defaults to '/'
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+
+app.use(express.static('public'));
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen(3000, function () {
+  console.log('App started on port 3000');
 });
